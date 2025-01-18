16cf60acd8517977d0a4164c9fcf2a01e5ba853f
Krish5706 <patelkrish5706@gmail.com>
2025-01-18 16:48:30 +0530

    update

--- new/script.js
+++ new/script.js
@@ -1,11 +1,17 @@
+import { attachEventListeners } from './firebase.js';
+
 const container = document.querySelector('.container');
 const registerbtn = document.querySelector('.register-btn');
 const loginbtn = document.querySelector('.login-btn');
 
-registerbtn.addEventListener('click', ()=>{
-    container.classList.add('active');
+// Toggle between login and registration forms
+registerbtn.addEventListener('click', () => {
+  container.classList.add('active');
 });
 
-loginbtn.addEventListener('click', ()=>{
-    container.classList.remove('active');
+loginbtn.addEventListener('click', () => {
+  container.classList.remove('active');
 });
+
+// Attach Firebase event listeners
+attachEventListeners();

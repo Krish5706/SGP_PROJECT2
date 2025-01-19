b00998269156ce43a0e860fc7bbe580ae00409ca
Krish5706 <patelkrish5706@gmail.com>
2025-01-19 10:52:22 +0530

    login update

--- Transceed Meet/script.js
+++ Transceed Meet/script.js
@@ -66,14 +66,14 @@
 
 // Firebase Configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBe0sg0snJDVjldfjG5fZX_kCibDNyDkwA",
-    authDomain: "transceed-meet.firebaseapp.com",
-    projectId: "transceed-meet",
-    storageBucket: "transceed-meet.firebasestorage.app",
-    messagingSenderId: "842320819809",
-    appId: "1:842320819809:web:b8eab84607741451c4dc20",
-    measurementId: "G-BHCFMTVV8S"
-};
+    apiKey: "AIzaSyDIJU-d_089-5LvDdLN-5W-KHgIeXr254E",
+    authDomain: "transceed-meet-fd843.firebaseapp.com",
+    projectId: "transceed-meet-fd843",
+    storageBucket: "transceed-meet-fd843.firebasestorage.app",
+    messagingSenderId: "927339188064",
+    appId: "1:927339188064:web:73d8c9c843da15112f9805",
+    measurementId: "G-TGKKF34QF9"
+  };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);

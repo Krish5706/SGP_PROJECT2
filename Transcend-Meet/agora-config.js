7f7029cee506568ac333f159c94c85f4d3fd2080
Prajapati Daksh <dakshprajapati2208@gmail.com>
2025-03-17 16:53:17 +0530

    push

---
+++ Transcend-Meet/agora-config.js
@@ -0,0 +1,6 @@
+// Agora configuration
+const AGORA_CONFIG = {
+    APP_ID: "2b05897bd3d24ed0b4ad08c4c1701cf4", // Replace with your Agora App ID
+    LOG_LEVEL: 1, // 0: DEBUG, 1: INFO, 2: WARNING, 3: ERROR, 4: NONE
+    USE_TOKEN_SERVER: false // Set to true if you're using token authentication
+};

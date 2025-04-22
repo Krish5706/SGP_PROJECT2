0cf7371229a248fcb97abd527d664bd0476826ba
Krish5706 <patelkrish5706@gmail.com>
2025-04-22 10:26:39 +0530

    token change

--- tarnscendmeet-agrora/script.js
+++ tarnscendmeet-agrora/script.js
@@ -4,7 +4,7 @@ let client = AgoraRTC.createClient({mode:'rtc', codec:"vp8"})
 //#2
 let config = {
     appid: '4d4d7cbbe8664bf4b5490ca3bc6a464e',
-    token: '007eJxTYFiUJ/9xqq1pimc7h4vkE9kfpyt/6H78HJ6We3vLKrs9xcoKDCYpJinmyUlJqRZmZiZJaSZJpiaWBsmJxknJZokmZiapN2uZMhoCGRmWpmgzMEIhiM/CkJuYmcfAAABufx/Q',
+    token: '007eJxTYOBlu/3+i1uFEJuAyjKXGFZlU01dlfjiL5m8Kd2u4TEnlRUYTFJMUsyTk5JSLczMTJLSTJJMTSwNkhONk5LNEk3MTFKDFNkzGgIZGVS2n2BkZIBAEJ+FITcxM4+BAQDQJhpw',
     uid:null,
     channel: 'main',
 }
